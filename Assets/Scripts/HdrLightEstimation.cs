3a21a07f2528a920bbc9af1514ad9d5130b8631c
Andreas Jakl <andreas.jakl@live.com>
2021-05-17 11:08:07 +0200

    Enable Light Estimation

---
+++ Assets/Scripts/HdrLightEstimation.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(Light))]
+public class HdrLightEstimation : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The ARCameraManager which will produce frame events containing light estimation information.")]
+    private ARCameraManager _cameraManager;
+
+    private Light _light;
+
+
+    void Awake()
+    {
+        _light = GetComponent<Light>();
+    }
+
+    void OnEnable()
+    {
+        if (_cameraManager != null)
+            _cameraManager.frameReceived += CameraFrameChanged;
+    }
+
+    void OnDisable()
+    {
+        if (_cameraManager != null)
+            _cameraManager.frameReceived -= CameraFrameChanged;
+    }
+
+    private void CameraFrameChanged(ARCameraFrameEventArgs args)
+    {
+        if (args.lightEstimation.averageBrightness.HasValue)
+        {
+            _light.intensity = args.lightEstimation.averageBrightness.Value;
+        }
+
+        if (args.lightEstimation.averageColorTemperature.HasValue)
+        {
+            _light.colorTemperature = args.lightEstimation.averageColorTemperature.Value;
+        }
+
+        if (args.lightEstimation.colorCorrection.HasValue)
+        {
+            _light.color = args.lightEstimation.colorCorrection.Value;
+        }
+
+        if (args.lightEstimation.mainLightDirection.HasValue)
+        {
+            var mainLightDirection = args.lightEstimation.mainLightDirection;
+            _light.transform.rotation = Quaternion.LookRotation(mainLightDirection.Value);
+        }
+
+        if (args.lightEstimation.mainLightColor.HasValue)
+        {
+            // Could overwrite colorCorrection if that was available
+            // (this value is usually the better choice)
+            _light.color = (Color) args.lightEstimation.mainLightColor;
+        }
+
+        if (args.lightEstimation.mainLightIntensityLumens.HasValue)
+        {
+            // Could overwrite averageBrightness if that was available
+            // (this value is usually the better choice)
+            _light.intensity = (float) args.lightEstimation.mainLightIntensityLumens;
+        }
+
+        if (args.lightEstimation.ambientSphericalHarmonics.HasValue)
+        {
+            RenderSettings.ambientMode = AmbientMode.Skybox;
+            RenderSettings.ambientProbe = args.lightEstimation.ambientSphericalHarmonics.Value;
+        }
+    }
+}
